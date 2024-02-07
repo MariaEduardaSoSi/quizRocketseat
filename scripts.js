1fd8db7c867f3304dd3dd09f5dedba1ed5133461
Maria Eduarda <eduardasousasilva2103@gmail.com>
2024-02-07 11:01:56 -0300

    criei o HTML e algumas perguntas do JavaScript

---
+++ scripts.js
@@ -0,0 +1,99 @@
+const perguntas = [
+  {
+    pergunta: "Qual é a forma correta de declarar uma variável em JavaScript?",
+    resposta: [
+      "var myVar = 10;",
+      "let myVar = 10;",
+      "const myVar = 10;",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Qual dos seguintes métodos é usado para adicionar um elemento ao final de um array em JavaScript?",
+    resposta: [
+      "push()",
+      "append()",
+      "addToEnd()",
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Qual é a saída do seguinte código: console.log(typeof [])?",
+    resposta: [
+      "array",
+      "object",
+      "undefined",
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Qual é a função usada para converter uma string em um número em JavaScript?",
+    resposta: [
+      "parseInt()",
+      "parseNumber()",
+      "convertToNumber()",
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Qual é a diferença entre '==' e '===' em JavaScript?",
+    resposta: [
+      "'==' compara apenas o valor, '===' compara o valor e o tipo de dados.",
+      "'==' compara apenas o tipo de dados, '===' compara o valor e o tipo de dados.",
+      "'==' e '===' são equivalentes em JavaScript.",
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Qual é a função usada para imprimir algo no console em JavaScript?",
+    resposta: [
+      "print()",
+      "log()",
+      "console.log()",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Qual método é usado para remover o último elemento de um array em JavaScript?",
+    resposta: [
+      "pop()",
+      "removeLast()",
+      "deleteLast()",
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "O que a função 'Math.random()' retorna?",
+    resposta: [
+      "Um número aleatório entre 0 (inclusive) e 1 (exclusivo).",
+      "Um número inteiro aleatório.",
+      "Um número aleatório entre 0 e 100.",
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Qual é o operador de negação em JavaScript?",
+    resposta: [
+      "&",
+      "!",
+      "~",
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Qual é o método usado para converter um objeto JavaScript em uma string JSON?",
+    resposta: [
+      "toJSON()",
+      "stringify()",
+      "parse()",
+    ],
+    correta: 1
+  },
+];
+
+const template = document .querySelector('template')
+const quizItem = template.content.cloneNode
+ 
+for(const item of perguntas){
+
+}
