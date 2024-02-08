8a16edadcd7cb152572f3a899ad1201045065d28
Maria Eduarda <eduardasousasilva2103@gmail.com>
2024-02-07 21:55:17 -0300

    terminei a parte de JavaScript

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const perguntas = [
   {
     pergunta: "Qual é a forma correta de declarar uma variável em JavaScript?",
-    resposta: [
+    respostas: [
       "var myVar = 10;",
       "let myVar = 10;",
       "const myVar = 10;",
@@ -10,7 +10,7 @@ const perguntas = [
   },
   {
     pergunta: "Qual dos seguintes métodos é usado para adicionar um elemento ao final de um array em JavaScript?",
-    resposta: [
+    respostas: [
       "push()",
       "append()",
       "addToEnd()",
@@ -19,7 +19,7 @@ const perguntas = [
   },
   {
     pergunta: "Qual é a saída do seguinte código: console.log(typeof [])?",
-    resposta: [
+    respostas: [
       "array",
       "object",
       "undefined",
@@ -28,7 +28,7 @@ const perguntas = [
   },
   {
     pergunta: "Qual é a função usada para converter uma string em um número em JavaScript?",
-    resposta: [
+    respostas: [
       "parseInt()",
       "parseNumber()",
       "convertToNumber()",
@@ -37,7 +37,7 @@ const perguntas = [
   },
   {
     pergunta: "Qual é a diferença entre '==' e '===' em JavaScript?",
-    resposta: [
+    respostas: [
       "'==' compara apenas o valor, '===' compara o valor e o tipo de dados.",
       "'==' compara apenas o tipo de dados, '===' compara o valor e o tipo de dados.",
       "'==' e '===' são equivalentes em JavaScript.",
@@ -46,7 +46,7 @@ const perguntas = [
   },
   {
     pergunta: "Qual é a função usada para imprimir algo no console em JavaScript?",
-    resposta: [
+    respostas: [
       "print()",
       "log()",
       "console.log()",
@@ -55,7 +55,7 @@ const perguntas = [
   },
   {
     pergunta: "Qual método é usado para remover o último elemento de um array em JavaScript?",
-    resposta: [
+    respostas: [
       "pop()",
       "removeLast()",
       "deleteLast()",
@@ -64,7 +64,7 @@ const perguntas = [
   },
   {
     pergunta: "O que a função 'Math.random()' retorna?",
-    resposta: [
+    respostas: [
       "Um número aleatório entre 0 (inclusive) e 1 (exclusivo).",
       "Um número inteiro aleatório.",
       "Um número aleatório entre 0 e 100.",
@@ -73,7 +73,7 @@ const perguntas = [
   },
   {
     pergunta: "Qual é o operador de negação em JavaScript?",
-    resposta: [
+    respostas: [
       "&",
       "!",
       "~",
@@ -82,7 +82,7 @@ const perguntas = [
   },
   {
     pergunta: "Qual é o método usado para converter um objeto JavaScript em uma string JSON?",
-    resposta: [
+    respostas: [
       "toJSON()",
       "stringify()",
       "parse()",
@@ -91,19 +91,38 @@ const perguntas = [
   },
 ];
 
-const quiz = document.querySelector('#quiz')
-const template = document.querySelector('template')
+const quiz = document.querySelector('#quiz');
+const template = document.querySelector('template');
 
- 
-for(const item of perguntas){
+const correta = new Set()
+const totalDePerguntas = perguntas.length
+const mostrarTotal = document.querySelector('#acertos span')
+mostrarTotal.textContent = correta.size + 'de' + totalDePerguntas
+
+for(const item of perguntas) {
   const quizItem = template.content.cloneNode(true)
-  quizItem.querySelector('h3').textContent = item.pergunta
-  quiz.appendChild(quizItem)
+  quizItem.querySelector('h3').textContent = item.pergunta    
 
   for(let resposta of item.respostas){
     const dt = quizItem.querySelector('dl dt').cloneNode(true)
     dt.querySelector('span').textContent = resposta
+    dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item))
+    dt.querySelector('input').value = item.respostas.indexOf(resposta)
+    dt.querySelector('input').onchange = (event) => {
+      const estaCorreta = event.target.value == item.correta
+      
+      correta.delete(item)
+      if(estaCorreta){
+        correta.add(item)
+      }
 
+      mostrarTotal.textContent = correta.size + ' de ' + totalDePerguntas
+    }
+    
     quizItem.querySelector('dl').appendChild(dt)
-  }
+  }  
+
+  quizItem.querySelector('dl dt').remove()
+
+  quiz.appendChild(quizItem)  
 }
